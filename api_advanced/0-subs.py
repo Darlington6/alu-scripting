a3e89b1bb2b44ab9df623804610fd42be75d8e7e
Darlington6 <d.tunyinko@alustudent.com>
2024-07-03 13:00:42 +0000

    How many subs?

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,33 +1,26 @@
 #!/usr/bin/python3
-"""Module"""
+# -*- coding: utf-8 -*-
+
+""" Returns the number of subscribers for a given subreddit. """
 
 import requests
 
-def number_of_subscribers(subreddit):
-    """Function that return the number of subscribers from REDDIT API"""
 
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    
-    # Set a custom User-Agent to avoid Too Many Requests error
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    
-    try:
-        # Make a GET request to the Reddit API
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        
-        # Check if the request was successful
-        if response.status_code == 200:
-            data = response.json()
-            # Return the number of subscribers
-            return data['data']['subscribers']
-        else:
-            # If the status code is not 200, return 0
-            return 0
-    except requests.RequestException:
-        # If any request exception occurs, return 0
+def number_of_subscribers(subreddit):
+    """ Returns the number of subscribers for a given subreddit. """
+
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = \
+        {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)\
+          AppleWebKit/537.36(KHTML, like Gecko) \
+         Chrome/90.0.4430.93 Safari/537.36'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        return response.json().get('data').get('subscribers')
+    else:
         return 0
 
-# Example usage
-if __name__ == "__main__":
-    subreddit = "python"
-    print(f"The number of subscribers in r/{subreddit} is: {number_of_subscribers(subreddit)}")
+
+if __name__ == '__main__':
+    print(number_of_subscribers)('programming')
+    print(number_of_subscribers)('not_a_valid_subreddit')
